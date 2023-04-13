52d77c316dff19ea538941959a38be52ea77f246
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-04-13 08:31:00 +0530

    started with the roll-dice implementation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+let  diceBtn= document.querySelector(".btn--roll");
+
+function changeImage(){
+    document.querySelector(".dice").src="new url";
+}
+
+diceBtn.addEventListener("click",changeImage);
