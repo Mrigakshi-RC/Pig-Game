5246b7fbfb96ee269ebc1259c77880a02c4d706f
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-04-18 12:40:11 +0530

    altered the hold and current score mechanisms

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 let  diceBtn= document.querySelector(".btn--roll");
 let holdBtn=document.querySelector(".btn--hold");
 let scoreTable={"user1":0,"user2":0};
+let currTable={"user1":0,"user2":0};
 let currentUser="user1";
 
-function changeImage(){
+function diceFunc(){
     let diceVal={1:"./dice-1.png",2:"./dice-2.png",3:"./dice-3.png",4:"./dice-4.png",5:"./dice-5.png",6:"./dice-6.png",}
     let score=Math.ceil(Math.random() * 6);
     document.querySelector(".dice").src=diceVal[score];
@@ -15,22 +16,40 @@ function changeImage(){
 }
 
 function updateScore(user,score){
-    scoreTable[user]+=score;
+    currTable[user]+=score;
     if (user==="user1")
-    document.querySelector("#score--0").innerText=scoreTable[user]+score;
+    document.querySelector("#current--0").innerText=currTable[user];
     else
-    document.querySelector("#score--1").innerText=scoreTable[user]+score;
+    document.querySelector("#current--1").innerText=currTable[user];
+    
+    if(scoreTable[user]>=20)
+    winGame();
 }
 
-function resetScore(user){
-    scoreTable[user]=0;
-    if (user==="user1"){
-        document.querySelector("#score--0").innerText=0;
+function holdScore(){
+    console.log(currTable)
+    scoreTable[currentUser]+=currTable[currentUser];
+
+    if (currentUser==="user1")
+    document.querySelector("#score--0").innerText=scoreTable[currentUser];
+    else
+    document.querySelector("#score--1").innerText=scoreTable[currentUser];
+    resetScore();
+
+    if(scoreTable[currentUser]>=20)
+    winGame();
+    
+    switchPlayer();
+}
+
+function resetScore(){
+    currTable[currentUser]=0;
+    if (currentUser==="user1"){
+        document.querySelector("#current--0").innerText=0;
     }
     else{
-        document.querySelector("#score--1").innerText=0;
+        document.querySelector("#current--1").innerText=0;
     }
-    switchPlayer();
 }
 
 function switchPlayer(){
@@ -46,13 +65,14 @@ function switchPlayer(){
     }
 }
 
-// function winGame(){
-//     if (currentUser==="user1"){
-//         currentUser="user2";
-//         document.querySelector(".player--1").classList.add("player--active");
-//         document.querySelector(".player--0").classList.remove("player--active");
-//     }
-// }
+function winGame(){
+    if (currentUser==="user1"){
+        document.querySelector(".player--0").classList.add("player--winner");
+    }
+    else{
+        document.querySelector(".player--1").classList.add("player--winner");
+    }
+}
 
-diceBtn.addEventListener("click",changeImage);
-holdBtn.addEventListener("click",switchPlayer);
+diceBtn.addEventListener("click",diceFunc);
+holdBtn.addEventListener("click",holdScore);
